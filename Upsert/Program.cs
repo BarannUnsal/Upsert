054642a1458ccee698e019e76e3033d19aefedc8
Barannunsal <barannunsal@gmail.com>
2024-12-22 19:49:05 +0300

    Initial commit: Add basic Program.cs file for application setup

---
+++ Upsert/Program.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Upsert.Context;
+using Upsert.Entities;
+using Upsert.Helpers;
+using Upsert.Repositories;
+using Upsert.Services;
+
+IConfigurationBuilder builder = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+IConfigurationRoot configuration = builder.Build();
+
+
+var services = new ServiceCollection()
+    .AddDbContext<AppDbContext>(opt =>
+    {
+        opt.UseNpgsql(configuration.GetConnectionString("Default"));
+    })
+    .AddTransient<IDbRepository, DbRepository>()
+    .AddTransient<IUpsertService, UpsertService>()
+    .BuildServiceProvider();
+
+IServiceScope scope = services.CreateScope();
+AppDbContext dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+dbContext.Database.Migrate();
+
+var requestedUser = new User
+{
+    Id = 1,
+    Name = "asdsadbgf nhgfb n gf gf "
+};
+
+await Upsert(requestedUser);
+
+async Task Upsert<T>(T table) where T : class
+{
+    List<(string ColumnName, string InsertValues)> class2InsertColumn = DbHelper.Class2Column(table, table);
+
+
+    (string TableName, string PrimaryKey) = DbHelper.GetTableName(dbContext.Model, table);
+
+    IUpsertService upsertService = scope.ServiceProvider.GetRequiredService<IUpsertService>();
+
+    await upsertService.Upsert<T>(
+         TableName,
+         class2InsertColumn,
+         PrimaryKey,
+         class2InsertColumn);
+}
